0f33c504a75bbc8143829bcc07515801357cad0b
GugaSantos <guga.santos.guga@gmail.com>
2021-12-02 16:29:30 -0300

    Resolucao do exercicio 07

--- src/challenges.js
+++ src/challenges.js
@@ -54,8 +54,18 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  let gato1 = Math.abs(mouse - cat1)
+  let gato2 = Math.abs(mouse - cat2)
+
+  if(gato1 < gato2) {
+    return "cat1"
+  } else if (gato2 < gato1) {
+    return "cat2"
+  } else {
+    return "os gatos trombam e o rato foge"
+  }
 }
 
 // Desafio 8
