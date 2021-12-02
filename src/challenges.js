b9d25ada94eb3b4d2903f3befd51d0f4ee06221f
GugaSantos <guga.santos.guga@gmail.com>
2021-12-02 15:59:49 -0300

    Resolucao exercicio 04

--- src/challenges.js
+++ src/challenges.js
@@ -22,8 +22,9 @@ function splitSentence(string) {
 }
 
 // Desafio 4
-function concatName() {
+function concatName(array) {
   // seu código aqui
+  return array[array.length -1] + ", " + array[0]
 }
 
 // Desafio 5
