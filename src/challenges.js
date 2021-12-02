cc1dd83dbe408cb866105f703435907cec7bf649
GugaSantos <guga.santos.guga@gmail.com>
2021-12-02 16:02:58 -0300

    Resolucao do exercicio 05

--- src/challenges.js
+++ src/challenges.js
@@ -28,8 +28,9 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  return (wins * 3) + (ties * 1)
 }
 
 // Desafio 6
