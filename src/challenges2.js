de911ed3318e68ae936fca3e032b7cb82af6d2b3
GugaSantos <guga.santos.guga@gmail.com>
2021-12-02 20:15:26 -0300

    Resolucao do exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,9 +16,30 @@ return objeto
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(array) {
   // seu código aqui
+  if(array.length !== 11) {
+    return "Array com tamanho incorreto."
+  }
+  let contador = 0
+  for(let i = 0; i < array.length; i += 1) {
+    for (let j = 0; j < array.length; j += 1){
+      if(array[i] === array[j]) {
+        contador += 1
+      }
+    }
+    if(array[i] > 9 || array[i] < 0 || contador >= 3) {
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+    contador = 0
+  }
+  array.splice(0,0,"(");
+  array.splice(3,0,")");
+  array.splice(4,0," ");
+  array.splice(10,0,"-");
+  return array.join('');
 }
+
 
 // Desafio 12
 function triangleCheck() {
